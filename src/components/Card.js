8c6345303fe64a58a100f04e994a878fbcf311a3
shirrigen <latuna2009@gmail.com>
2018-12-14 13:25:09 +0800

    retry zmage

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Zmage from 'react-zmage'
 import './Cards.css'
 
 const Card = props => (
     <div className="Card">
-        <img src={props.image} alt={props.alt} radius={16} />
+        <Zmage src={props.image} alt={props.alt} radius={16} />
         <h3>{props.title}</h3>
         <p>{props.text}</p>
     </div>
