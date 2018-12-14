fe19dc40abfffb04b85cbe2ca1d24f0867c34f07
shirrigen <latuna2009@gmail.com>
2018-12-14 13:17:42 +0800

    uninstall zmage

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,3 @@
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-    if (stage === "build-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /bad-module/,
-              use: loaders.null(),
-            },
-          ],
-        },
-      })
-    }
-  }
-
 /**
  * Implement Gatsby's Node APIs in this file.
  *
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Cards.css'
-import Zmage from 'react-zmage'
+// import Zmage from 'react-zmage'
 
 const Card = props => (
     <div className="Card">
-        <Zmage src={props.image} alt={props.alt} radius={16} />
+        <img src={props.image} alt={props.alt} radius={16} />
         <h3>{props.title}</h3>
         <p>{props.text}</p>
     </div>
